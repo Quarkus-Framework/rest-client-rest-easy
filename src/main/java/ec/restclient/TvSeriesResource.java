3ae1f1752ab89837115ef4824c4c3bc07ccea8b9
enrico84 <enrico8484@gmail.com>
2023-01-25 12:49:23 +0100

    New commit

--- src/main/java/ec/restclient/TvSeriesResource.java
+++ src/main/java/ec/restclient/TvSeriesResource.java
@@ -4,6 +4,7 @@ import ec.restclient.model.Episode;
 import ec.restclient.model.TvSerie;
 import ec.restclient.proxy.EpisodeProxy;
 import ec.restclient.proxy.TvSeriesProxy;
+import org.eclipse.microprofile.faulttolerance.Fallback;
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.media.Content;
 import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
@@ -50,12 +51,47 @@ public class TvSeriesResource {
                     required = true)
             @QueryParam("title") String title
     ) {
-        TvSerie tvSerie = tvSeriesProxyproxy.get(title);
-        List<Episode> episodeList = episodeProxy.get(tvSerie.getId());
+        TvSerie tvSerie = this.getTvSeries(title);
+        List<Episode> episodeList = this.getEpisodies(tvSerie.getId());
         //tvSeries.add(tvSerie);
         //return Response.ok(tvSeries).build();
 
         return Response.ok(episodeList).build();
+    }
+
+    /**
+     * Method called with @Fallback annotation for to handle a fallback
+     * It have to has public
+     */
+    @Fallback(fallbackMethod = "fallBackGetTvSeries")
+    public TvSerie getTvSeries(String title) {
+        return tvSeriesProxyproxy.get(title);
+    }
+
+    /*
+     * Fallback method have to has same signature of calling method
+     */
+    private TvSerie fallBackGetTvSeries(String title) {
+        TvSerie tvSerie = new TvSerie();
+        tvSerie.setId(1L);
+        return tvSerie;
+    }
+
+
+    /**
+     * Method called with @Fallback annotation for to handle a fallback
+     * It have to has public
+     */
+    @Fallback(fallbackMethod = "fallBackGetEpisodes")
+    public List<Episode> getEpisodies(Long id) {
+        return episodeProxy.get(id);
+    }
+
+    /*
+     * Fallback method have to has same signature of calling method
+     */
+    private List<Episode> fallBackGetEpisodes(Long id) {
+        return new ArrayList<>();
     }
 
 }
